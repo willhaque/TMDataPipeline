f674e217d0d5c7c5de18235965e680f7726ddff8
willhaque <81592828+willhaque@users.noreply.github.com>
2021-11-05 11:08:59 -0400

    Test java 2 files added

--- TestJava.java
+++ TestJava.java
@@ -1,5 +1,10 @@
 public class TestJava{
 public static void main(String[] args){
 System.out.println("Application Dev");
+for(int i =1;i<11;i++){
+for (int j=11;j>=1;j--){
+System.out.print("\t"+ i*j);
+} System.out.println();
+}
 }
 }
