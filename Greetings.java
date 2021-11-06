c26d679363d2aef2a836679426247a9027f0c93b
willhaque <81592828+willhaque@users.noreply.github.com>
2021-11-05 20:15:06 -0400

    added Greetings.java and class

---
+++ Greetings.java
@@ -0,0 +1,10 @@
+public class Greetings{
+	public static void main(String[] args){
+		System.out.println("Hello from Greetings");
+	greet("Happy new Year");
+
+	}
+	static void greet(String str){
+	System.out.println("!!!==>"+str + "<==!!!");
+	}
+}
